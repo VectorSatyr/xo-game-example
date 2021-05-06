cbb0a13d9edb79ab46c87394789d574bf2cca3cd
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-06 16:21:47 -0700

    Fix Board::~Board typo

--- include/sdl_xo_game/board.hpp
+++ include/sdl_xo_game/board.hpp
@@ -5,7 +5,7 @@ namespace Game
 {
 	struct __declspec(novtable) Board
 	{
-		virtual ~board() = default;
+		virtual ~Board() = default;
 		virtual std::size_t lines(std::size_t value, std::size_t length) const = 0;
 		virtual void place(std::size_t value, std::size_t x, std::size_t y) = 0;
 		virtual bool full() const = 0;
