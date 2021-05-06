71c8df76761121647722d7fdfc06ec6c68eb0b26
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-06 10:56:01 -0700

    Add placeholder console application
    This commit reflects the desired folder structure for future code
    changes.

---
+++ apps/app.cpp
@@ -0,0 +1,12 @@
+﻿// app.cpp : Defines the entry point for the application.
+//
+
+#include "sdl_xo_game/game.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
