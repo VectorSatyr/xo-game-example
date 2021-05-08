7d667e8c00b031c2580e2959d8340f3fbe713239
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-08 13:59:30 -0700

    Add XOBoard::full false test
    XOBoard::full should return false if one or more cells of the board are
    empty.

--- tests/unit/xo_board.cpp
+++ tests/unit/xo_board.cpp
@@ -7,3 +7,10 @@ TEST_CASE("XOBoard::full returns true when every cell of the board is occupied")
 	auto expected = true;
 	REQUIRE(actual == expected);
 }
+
+TEST_CASE("XOBoard::full returns false if even just one cell of the board is empty")
+{
+	auto actual = Game::XOBoard(3, 3, { 1, 1, 1, 0, 1, 1, 1, 1, 1 }).full();
+	auto expected = false;
+	CHECK(actual == expected);
+}
