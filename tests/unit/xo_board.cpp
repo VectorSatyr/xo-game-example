13f68a8c96dbf1d38b766392ba8b5b377bac85ac
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-08 14:36:46 -0700

    Adjust code to fit 80 character column width
    The test data was also rearranged to give a better visual of the logical
    state of Game::XOBoard.

--- tests/unit/xo_board.cpp
+++ tests/unit/xo_board.cpp
@@ -1,16 +1,24 @@
 #include <doctest/doctest.h>
 #include "sdl_xo_game/xo_board.hpp"
 
-TEST_CASE("XOBoard::full returns true when every cell of the board is occupied")
+TEST_CASE("XOBoard::full returns true when all cells are occupied")
 {
-	auto actual = Game::XOBoard(3, 3, { 1, 1, 1, 1, 1, 1, 1, 1, 1 }).full();
+	auto actual = Game::XOBoard(3, 3, { 
+		1, 1, 1, 
+		1, 1, 1, 
+		1, 1, 1 
+	}).full();
 	auto expected = true;
 	REQUIRE(actual == expected);
 }
 
-TEST_CASE("XOBoard::full returns false if even just one cell of the board is empty")
+TEST_CASE("XOBoard::full returns false when any cell is empty")
 {
-	auto actual = Game::XOBoard(3, 3, { 1, 1, 1, 0, 1, 1, 1, 1, 1 }).full();
+	auto actual = Game::XOBoard(3, 3, { 
+		1, 1, 1, 
+		0, 1, 1, 
+		1, 1, 1 
+	}).full();
 	auto expected = false;
 	CHECK(actual == expected);
 }
