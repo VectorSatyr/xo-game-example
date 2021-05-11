875748cea50ef74544302fa3872a807f7d835434
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-11 15:29:45 -0700

    Improve XOBoard::lines naive test
    The test data now covers all valid line types.

--- tests/unit/xo_board.cpp
+++ tests/unit/xo_board.cpp
@@ -26,10 +26,10 @@ TEST_CASE("XOBoard::full returns false when any cell is empty")
 TEST_CASE("XOBoard::lines returns total unique lines of value and length")
 {
 	auto actual = Game::XOBoard(3, 3, { 
-		0, 1, 1, 
+		1, 1, 1, 
 		1, 1, 0, 
-		1, 1, 1 
+		1, 0, 1
 	}).lines(1, 3);
-	auto expected = 3;
+	auto expected = 4;
 	REQUIRE(actual == expected);
 }
