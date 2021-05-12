4412c0d21c61a02892af851b81fcd2dcee23149e
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-11 17:36:54 -0700

    Pass XOBoard::lines naive test

--- src/xo_board.cpp
+++ src/xo_board.cpp
@@ -17,7 +17,48 @@ namespace Game
 
 	std::size_t XOBoard::lines(std::size_t value, std::size_t length) const
 	{
-		return 0;
+		const std::size_t size(width * height);
+		std::size_t total(0), count(0);
+
+		// south-east line
+		for (std::size_t n = 0; n < size; n += (width + 1)) {
+			if (cells[n] == value) count++;
+		}
+		if (count == length) total++;
+		count = 0;
+
+		// south-west line
+		for (auto n = width - 1; n < size - 1; n += (width - 1)) {
+			if (cells[n] == value) count++;
+		}
+		if (count == length) total++;
+		count = 0;
+
+		// rows
+		const auto pred = [&value](const std::size_t n) noexcept {
+			return n == value;
+		};
+		for (auto it = cells.cbegin();
+			it != cells.cend();
+			it += width) {
+			if (std::count_if(it, it + width, pred) == length) {
+				total++;
+			}
+		}
+
+		// columns
+		std::vector<std::size_t> columns;
+		for (std::size_t n = 0; n < size; n++) {
+			columns.push_back(cells[(n * width) % (size - 1)]);
+		}
+		for (auto it = columns.cbegin();
+			it != columns.cend();
+			it += width) {
+			if (std::count_if(it, it + width, pred) == length) {
+				total++;
+			}
+		}
+		return total;
 	}
 
 	void XOBoard::place(std::size_t value, std::size_t x, std::size_t y)
