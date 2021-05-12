d482ef4d03234ff8fad72dd36b17543d9923494d
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-12 12:41:58 -0700

    Pass XOBoard::lines length underflow width test
    Refactoring required.

--- src/xo_board.cpp
+++ src/xo_board.cpp
@@ -17,47 +17,73 @@ namespace Game
 
 	std::size_t XOBoard::lines(std::size_t value, std::size_t length) const
 	{
-		const std::size_t size(width * height);
-		std::size_t total(0), count(0);
+		std::size_t total = 0;
 
-		// south-east line
-		for (std::size_t n = 0; n < size; n += (width + 1)) {
-			if (cells[n] == value) count++;
-		}
-		if (count == length) total++;
-		count = 0;
+		if (length > 0) {
+			const std::size_t size(cells.size());
+			std::vector<std::vector<std::size_t>> lines;
 
-		// south-west line
-		for (auto n = width - 1; n < size - 1; n += (width - 1)) {
-			if (cells[n] == value) count++;
-		}
-		if (count == length) total++;
-		count = 0;
+			const auto w(width - length + 1);
+			const auto h(height - length + 1);
 
-		// rows
-		const auto pred = [&value](const std::size_t n) noexcept {
-			return n == value;
-		};
-		for (auto it = cells.cbegin();
-			it != cells.cend();
-			it += width) {
-			if (std::count_if(it, it + width, pred) == length) {
-				total++;
+			// south-east line(s)
+			for (std::size_t y = 0; y < h; ++y) {
+				for (std::size_t x = 0; x < w; ++x) {
+					std::vector<std::size_t> line;
+					for (std::size_t n = 0; n < length; ++n) {
+						auto index = (y * width) + x + (n * (width + 1));
+						if (index < size) line.push_back(cells.at(index));
+					}
+					if (line.size() == length) lines.push_back(line);
+				}
+			}
+
+			// south-west line(s)
+			for (std::size_t y = 0; y < h; ++y) {
+				for (std::size_t x = 0; x < w; ++x) {
+					std::vector<std::size_t> line;
+					for (std::size_t n = 0; n < length; ++n) {
+						auto index = (y * width) + (length - 1) + x + (n * (width - 1));
+						if (index < size) line.push_back(cells.at(index));
+					}
+					if (line.size() == length) lines.push_back(line);
+				}
+			}
+
+			// horizontal line(s)
+			for (std::size_t y = 0; y < height; ++y) {
+				for (std::size_t x = 0; x < w; ++x) {
+					std::vector<std::size_t> line;
+					for (std::size_t n = 0; n < length; ++n) {
+						auto index = (y * width) + x + n;
+						if (index < size) line.push_back(cells.at(index));
+					}
+					if (line.size() == length) lines.push_back(line);
+				}
+			}
+
+			// vertical line(s)
+			for (std::size_t y = 0; y < h; ++y) {
+				for (std::size_t x = 0; x < width; ++x) {
+					std::vector<std::size_t> line;
+					for (std::size_t n = 0; n < length; ++n) {
+						auto index = (y * width) + x + (n * width);
+						if (index < size) line.push_back(cells.at(index));
+					}
+					if (line.size() == length) lines.push_back(line);
+				}
+			}
+
+			// matches
+			const auto pred = [&value](const std::size_t n) noexcept {
+				return n == value;
+			};
+			for (const auto& line : lines) {
+				if (std::all_of(line.cbegin(), line.cend(), pred)) total++;
 			}
 		}
 
-		// columns
-		std::vector<std::size_t> columns;
-		for (std::size_t n = 0; n < size; n++) {
-			columns.push_back(cells[(n * width) % (size - 1)]);
-		}
-		for (auto it = columns.cbegin();
-			it != columns.cend();
-			it += width) {
-			if (std::count_if(it, it + width, pred) == length) {
-				total++;
-			}
-		}
+		// finished
 		return total;
 	}
 
