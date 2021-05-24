85e56f6baa57aac43b3a55ecd4f9b9e1f292a400
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-24 15:42:19 -0700

    Create iota.hpp
    Iota is the ninth letter of the Greek alphabet, used here to represent
    a sequence of consecutive integers.

---
+++ include/utils/iota.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "sequence.hpp"
+#include <numeric>
+
+namespace utils
+{
+	template<class T>
+	class iota final : public sequence<T>
+	{
+	public:
+		using size_type = typename std::vector<T>::size_type;
+
+		explicit iota(const size_type len) : Iota(len, 0) {};
+
+		explicit iota(const size_type len, const T start) :
+			len(len), start(start) {};
+
+		std::vector<T> vector() const override
+		{
+			std::vector<T> result(len);
+			std::iota(result.begin(), result.end(), start);
+			return result;
+		};
+
+	private:
+		const size_type len;
+		const T start;
+	};
+}
