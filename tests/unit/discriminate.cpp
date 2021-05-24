726a970f389b5fe3bddf4bc68e4cfccd759c99bb
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-24 15:10:12 -0700

    Refactor Game::Discriminate tests

--- tests/unit/discriminate.cpp
+++ tests/unit/discriminate.cpp
@@ -3,22 +3,20 @@
 
 TEST_CASE("Discriminate<T>::vector returns a discriminated vector of T")
 {
+	using size = Game::Discriminate<int>::size_type;
 	const auto actual = Game::Discriminate<int>(
 		{ 7, 6, 5, 4, 3, 2, 1, 0 }, 4, 
-		[](const Game::Discriminate<int>::size_type n) noexcept {
-			return 6 - n;
-		}).vector();
+		[](const size n) noexcept { return 6 - n; }).vector();
 	const auto expected = std::vector<int>{ 1, 2, 3, 4 };
 	REQUIRE(actual == expected);
 }
 
 TEST_CASE("Discriminate<T>::vector returns only elements in range")
 {
+	using size = Game::Discriminate<unsigned int>::size_type;
 	const auto actual = Game::Discriminate<unsigned int>(
 		{ 2, 4, 6, 8 }, 10,
-		[](const Game::Discriminate<unsigned int>::size_type n) noexcept {
-			return n + 3;
-		}).vector();
+		[](const size n) noexcept { return n + 3; }).vector();
 	const auto expected = std::vector<unsigned int>{ 8 };
 	REQUIRE(actual == expected);
 }
