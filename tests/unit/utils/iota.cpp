1a037926b0831f02b249c5a02f06658f0ea00c73
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-24 16:02:57 -0700

    Add iota<T>::vector default constructor test
    utils::iota<T>::vector will start from 0 if no start value is
    specified.

--- tests/unit/utils/iota.cpp
+++ tests/unit/utils/iota.cpp
@@ -7,3 +7,10 @@ TEST_CASE("iota<T>::vector returns an array of consecutive T values")
 	const auto expected = std::vector<int>{ 2, 3, 4, 5, 6, 7, 8 };
 	REQUIRE(actual == expected);
 }
+
+TEST_CASE("iota<T>::vector starts from 0 by default")
+{
+	const auto actual = utils::iota<unsigned int>(5).vector();
+	const auto expected = std::vector<unsigned int>{ 0, 1, 2, 3, 4 };
+	REQUIRE(actual == expected);
+}
