c9a45b69f6840de91fc5d08f340d73baaf681e02
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-24 17:17:03 -0700

    Fix transformed<T>::vector basic test
    utils::transformed<T> is decorator of utils::sequence<T>; as such, it
    should accept a reference to an existing utils::sequence<T>. (It may
    wrap a vector in another utils::sequence<T> implementation, if
    required)

--- tests/unit/utils/transformed.cpp
+++ tests/unit/utils/transformed.cpp
@@ -1,11 +1,11 @@
 #include <doctest/doctest.h>
 #include "utils/transformed.hpp"
+#include "utils/iota.hpp"
 
 TEST_CASE("transformed<T>::vector applies a function to a sequence of T")
 {
 	const auto actual = utils::transformed<int>(
-		std::vector<int>{ 0, 1, 2, 3 }, 
-		[](const auto n) noexcept { return n * 3; }
+		utils::iota<int>(4), [](const auto n) noexcept { return n * 3; }
 	).vector();
 	const auto expected = std::vector<int>{ 0, 3, 6, 9 };
 	REQUIRE(actual == expected);
