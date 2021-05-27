77b61b2c62d6c35bcfa5b13da570c82fa44b4da6
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-05-27 11:42:43 -0700

    Fix typo in transformed<T>::vector basic test

--- tests/unit/utils/transformed.cpp
+++ tests/unit/utils/transformed.cpp
@@ -6,6 +6,6 @@ TEST_CASE("transformed<T>::vector applies a function to a sequence of T")
 	const auto actual = utils::transformed<int>(
 		{ 1, 3, 2, 5 }, [](const auto n) noexcept { return n * 3; }
 	).vector();
-	const auto expected = std::vector<int>{ 3, 9, 6, 12 };
+	const auto expected = std::vector<int>{ 3, 9, 6, 15 };
 	REQUIRE(actual == expected);
 }
