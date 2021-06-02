72eab14d888b17ded2923a416d75a137e67af41b
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-06-02 10:23:37 -0700

    Improve mapped<T> tests organization
    When adding tests to test suites, those regarding mapped<T> were
    accidentally overlooked. These tests are now properly organized.

--- tests/unit/utils/mapped.cpp
+++ tests/unit/utils/mapped.cpp
@@ -1,20 +1,26 @@
 #include <doctest/doctest.h>
 #include "utils/mapped.hpp"
 
-TEST_CASE("mapped<T>::vector returns a mapped vector of T")
+TEST_SUITE("mapped<T>")
 {
-	const auto actual = utils::mapped<int>(
-		{ 7, 6, 5, 4, 3, 2, 1, 0 }, 4, 
-		[](const auto n) noexcept { return 6 - n; }).vector();
-	const auto expected = std::vector<int>{ 1, 2, 3, 4 };
-	REQUIRE(actual == expected);
-}
+	TEST_SUITE("mapped<T>::vector()")
+	{
+		TEST_CASE("Returns a mapped vector of T elements")
+		{
+			const auto actual = utils::mapped<int>(
+				{ 7, 6, 5, 4, 3, 2, 1, 0 }, 4,
+				[](const auto n) noexcept { return 6 - n; }).vector();
+			const auto expected = std::vector<int>{ 1, 2, 3, 4 };
+			REQUIRE(actual == expected);
+		}
 
-TEST_CASE("mapped<T>::vector returns only elements in range")
-{
-	const auto actual = utils::mapped<unsigned int>(
-		{ 2, 4, 6, 8 }, 10,
-		[](const auto n) noexcept { return n + 3; }).vector();
-	const auto expected = std::vector<unsigned int>{ 8 };
-	REQUIRE(actual == expected);
+		TEST_CASE("Returns only elements in range of the given vector")
+		{
+			const auto actual = utils::mapped<unsigned int>(
+				{ 2, 4, 6, 8 }, 10,
+				[](const auto n) noexcept { return n + 3; }).vector();
+			const auto expected = std::vector<unsigned int>{ 8 };
+			REQUIRE(actual == expected);
+		}
+	}
 }
