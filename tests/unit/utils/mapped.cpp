abe6ad1139fb022b3190db6dd8c16f6aa2c223bc
Joevan Salmon-Johnson <15187586+VectorSatyr@users.noreply.github.com>
2021-06-10 10:52:04 -0700

    Add template test cases of mapped<T>
    The utils::mapped<T> test cases are now templated, enabling testing
    against multiple value types. The descriptions of these cases were also
    slightly modified to accomodate the concatenation of template
    information.

--- tests/unit/utils/mapped.cpp
+++ tests/unit/utils/mapped.cpp
@@ -5,22 +5,32 @@ TEST_SUITE("mapped<T>")
 {
 	TEST_SUITE("mapped<T>::vector()")
 	{
-		TEST_CASE("Returns a mapped vector of T elements")
+		TEST_CASE_TEMPLATE_DEFINE(
+			"Returns a mapped vector", T, mapped_default
+		)
 		{
-			const auto actual = utils::mapped<int>(
+			const auto actual = utils::mapped<T>(
 				{ 7, 6, 5, 4, 3, 2, 1, 0 }, 4,
 				[](const auto n) noexcept { return 6 - n; }).vector();
-			const auto expected = std::vector<int>{ 1, 2, 3, 4 };
+			const auto expected = std::vector<T>{ 1, 2, 3, 4 };
 			REQUIRE(actual == expected);
 		}
 
-		TEST_CASE("Returns only elements in range of the given vector")
+		TEST_CASE_TEMPLATE_DEFINE(
+			"Returns only elements in range of the given vector", 
+			T, mapped_in_range
+		)
 		{
-			const auto actual = utils::mapped<unsigned int>(
+			const auto actual = utils::mapped<T>(
 				{ 2, 4, 6, 8 }, 10,
 				[](const auto n) noexcept { return n + 3; }).vector();
-			const auto expected = std::vector<unsigned int>{ 8 };
+			const auto expected = std::vector<T>{ 8 };
 			REQUIRE(actual == expected);
 		}
+
+		TEST_CASE_TEMPLATE_INVOKE(
+			mapped_default, int, unsigned int, std::size_t);
+		TEST_CASE_TEMPLATE_INVOKE(
+			mapped_in_range, int, unsigned int, std::size_t);
 	}
 }
